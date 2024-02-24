7dd77a998ded70ef98c02cade9fac5df2e98da66
chihebgmc <chihebchabougmc@gmail.com>
2024-02-24 17:18:51 +0100

    coding session 24/02/2024

--- src/features/orders/orderService.js
+++ src/features/orders/orderService.js
@@ -2,6 +2,30 @@ import axios from 'axios';
 
 const API_URL = '/api/orders/';
 
-const orderService = {};
+// Create order service
+const createOrder = async orderData => {
+  const response = await axios.post(API_URL, orderData);
+  return response.data;
+};
+
+// Get all orders service
+const getAllOrders = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+// Get one order service
+const getOrder = async _id => {
+  const response = await axios.get(API_URL + _id);
+  return response.data;
+};
+
+// Update order service
+const updateOrder = async _id => {
+  const response = await axios.put(API_URL + _id);
+  return response.data;
+};
+
+const orderService = { createOrder, getAllOrders, getOrder, updateOrder };
 
 export default orderService;
--- src/features/orders/orderSlice.js
+++ src/features/orders/orderSlice.js
@@ -1,20 +1,150 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import orderService from './orderService';
 
 const initialState = {
   ordersList: [],
+  order: null,
   isError: false,
   isSuccess: false,
   isLoading: false,
   message: '',
 };
 
+// Create order
+export const createOrder = createAsyncThunk(
+  'order/create',
+  async (orderData, thunkAPI) => {
+    try {
+      return await orderService.createOrder(orderData);
+    } catch (error) {
+      const message =
+        (error.message && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Get all orders
+export const getAllOrders = createAsyncThunk(
+  'order/getAll',
+  async (_, thunkAPI) => {
+    try {
+      return await orderService.getAllOrders();
+    } catch (error) {
+      const message =
+        (error.message && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Get one order
+export const getOrder = createAsyncThunk(
+  'order/getOne',
+  async (_id, thunkAPI) => {
+    try {
+      return await orderService.getOrder(_id);
+    } catch (error) {
+      const message =
+        (error.message && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Update order
+export const updateOrder = createAsyncThunk(
+  'order/update',
+  async (_id, thunkAPI) => {
+    try {
+      return await orderService.updateOrder(_id);
+    } catch (error) {
+      const message =
+        (error.message && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 // Create Order Slice
 const orderSlice = createSlice({
   name: 'order',
   initialState,
-  reducers: {},
-  extraReducers: builder => {},
+  reducers: {
+    reset: state => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = '';
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(createOrder.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.ordersList.push(action.payload);
+      })
+      .addCase(createOrder.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getAllOrders.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrders.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.ordersList = action.payload;
+      })
+      .addCase(getAllOrders.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getOrder.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(getOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.order = action.payload;
+      })
+      .addCase(getOrder.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(updateOrder.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(updateOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.ordersList.map(order =>
+          order._id === action.payload._id ? action.payload : order
+        );
+        state.order = action.payload;
+      })
+      .addCase(updateOrder.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
-export const {} = orderSlice.actions;
+export const { reset } = orderSlice.actions;
 export default orderSlice.reducer;
